9b4759801e1c4c70387eb2cec97d80edc2c0cc82
ruilinyou <chinaryan@outlook.com>
2023-01-28 08:58:51 -0700

    update

--- followingmouse.js
+++ followingmouse.js
@@ -8,3 +8,15 @@ document.addEventListener('mousemove', e => {
     circle.style.top = y + 'px';
   });
 });
+const dots = document.querySelectorAll('.dot');
+
+// get a random number between min and max
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+dots.forEach(dot => {
+  dot.style.left = getRandomInt(0, window.innerWidth) + 'px';
+  dot.style.top = getRandomInt(0, window.innerHeight) + 'px';
+  dot.style.transform = `scale(${getRandomInt(1,10) * 0.1})`;
+});
